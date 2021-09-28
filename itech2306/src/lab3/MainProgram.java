641e42c256837c5496092b30ab71f2a0f6df65a0
ZhehongZhuang <2444186732@qq.com>
2021-09-28 21:58:55 +0800

    task2.2

--- itech2306/src/lab3/MainProgram.java
+++ itech2306/src/lab3/MainProgram.java
@@ -3,6 +3,6 @@ package lab3;
 public class MainProgram {
 	public static void main(String[] args) {
 		System.out.println("The following people have been here to edit this file: ");   
-	    System.out.println("That is all!!");
+	    System.out.println("That is all!!!!");
 	}
 }
