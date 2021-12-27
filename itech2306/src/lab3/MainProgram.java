cb0cfbd548afbc4cc9aed876a3afdd78bc17641c
QinfengHuang <71870124+QinfengHuang@users.noreply.github.com>
2021-12-27 17:55:38 +0800

    Update MainProgram.java

--- itech2306/src/lab3/MainProgram.java
+++ itech2306/src/lab3/MainProgram.java
@@ -3,6 +3,7 @@ package lab3;
 public class MainProgram {
 	public static void main(String[] args) {
 		System.out.println("The following people have been here to edit this file: ");   
+		System.out.println("yes!! ");   
 	    System.out.println("That is all!!!!!!");
 	}
 }
